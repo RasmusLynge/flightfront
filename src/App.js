ec784d501f71771b3bd065647211901acd59c680
RasmusLynge <RasmuslJacobsen@live.com>
2019-05-02 13:34:44 +0200

    jumbotron

--- src/App.js
+++ src/App.js
@@ -8,57 +8,69 @@ import {
   Button,
   InputGroup,
   Form,
-  FormControl, 
+  FormControl,
   Table
 } from "react-bootstrap";
 import FetchFlights from "./FetchFlights";
 import "./App.css";
 
 function RowItem(props) {
-  return <tr>
-          <td>{props.value.startCountry}</td>
-          <td>{props.value.endCountry}</td>
-          <td>{props.value.departureDate}</td>
-          <td>{props.value.departureTime}</td>
-          <td>{props.value.duration}</td>
-          <td>{props.value.numberOfSeats}</td>
-          <td>{props.value.airline}</td>
-          <td>{props.value.price}</td>
-        </tr>;
+  return (
+    <tr>
+      <td>{props.value.startCountry}</td>
+      <td>{props.value.endCountry}</td>
+      <td>{props.value.departureDate}</td>
+      <td>{props.value.departureTime}</td>
+      <td>{props.value.duration}</td>
+      <td>{props.value.numberOfSeats}</td>
+      <td>{props.value.airline}</td>
+      <td>{props.value.price}</td>
+    </tr>
+  );
 }
 function MakeFlightTable(props) {
   const flights = props.flights;
-  const thead = <thead>
-                  <tr>
-                    <th>To</th>
-                    <th>From</th>
-                    <th>Date</th>
-                    <th>Departure time</th>
-                    <th>Duration</th>
-                    <th>Seats left</th>
-                    <th>Airlines</th>
-                    <th>Prices</th>
-                  </tr>
-                </thead>
-  const RowItems = flights.map((f) =>
-    <RowItem key={f.id} value={f} />);
-  return <Table>{thead}<tbody>{RowItems}</tbody></Table>;
+  const thead = (
+    <thead>
+      <tr>
+        <th>To</th>
+        <th>From</th>
+        <th>Date</th>
+        <th>Departure time</th>
+        <th>Duration</th>
+        <th>Seats left</th>
+        <th>Airlines</th>
+        <th>Prices</th>
+      </tr>
+    </thead>
+  );
+  const RowItems = flights.map(f => <RowItem key={f.id} value={f} />);
+  return (
+    <Table striped bordered hover size="sm">
+      {thead}
+      <tbody>{RowItems}</tbody>
+    </Table>
+  );
 }
-
 
 function TableDemo(props) {
   if (props.flights.length > 0) {
     return (
-      <div>
+      <Jumbotron>
         <h2>All Flights</h2>
         <MakeFlightTable flights={props.flights} />
-      </div>
+      </Jumbotron>
     );
+  } else if (props.flights.msg === "No flights") {
+    return (
+      <Jumbotron>
+        <h2>No Flights for this day</h2>
+      </Jumbotron>)
   } else {
     return (
-      <div>
+      <Jumbotron>
         <h2>Search for Flights</h2>
-      </div>
+      </Jumbotron>
     );
   }
 }
@@ -87,35 +99,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Navbar.Brand href="#home">Scandinair</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link eventKey={2} href="#memes">
-                Login
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-
+        <NavbarTop />
         <Navbar className="bg-light justify-content-between">
           <Form inline onSubmit={this.search} onChange={this.onChange}>
             <InputGroup>
@@ -150,13 +134,41 @@ class App extends React.Component {
             <Button type="submit">Submit</Button>
           </Form>
         </Navbar>
-
-        <Jumbotron>
-          <TableDemo flights={this.state.flightData} />
-        </Jumbotron>
+        <TableDemo flights={this.state.flightData} />
       </div>
     );
   }
+}
+
+function NavbarTop() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="#home">Scandinair</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Nav>
+          <Nav.Link eventKey={2} href="#memes">
+            Login
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
 export default App;
